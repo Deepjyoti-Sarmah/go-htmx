7551e8e3b561337039f8a2a533c1f161ca9f7a65
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-08-31 11:41:01 +0530

    adding bootstrap and templating

--- main.go
+++ main.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	
 )
+
+type Film struct {
+  Title string
+  Director string
+}
 
 func main() {
   fmt.Println("hello world")
 
-  http.ListenAndServe(":8000", nil)
+  h1 := func (w http.ResponseWriter, r *http.Request)  {
+    tmp1 := template.Must(template.ParseFiles("index.html"))
+    films := map[string][]Film{
+      "Films" : {
+        {Title: "The Godfather", Director: "Francis Ford Coppola"},
+        {Title: "Blade Runner", Director: "Ridley Scott"},
+        {Title: "The Thing", Director: "John Carpenter"},
+      },
+    }
+    tmp1.Execute(w, films)
+  }
+  http.HandleFunc("/",h1)
+
+  log.Fatal(http.ListenAndServe(":8000", nil))
+
 }
